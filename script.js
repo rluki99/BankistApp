dd0e27e00a943a43594784de0977e9e5e9aa01dc
rluki99 <lukasz.beczek24@gmail.com>
2023-10-11 13:41:07 +0200

    feat: calculating and displaying acc balance (reduce func on movements)

--- script.js
+++ script.js
@@ -78,23 +78,24 @@ const displayMovements = movements => {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-
 displayMovements(account1.movements);
 
-const createUsernames = (accounts) => {
+const calcDisplayBalance = movements => {
+  const balance = movements.reduce((acc, mov) => acc + mov);
+  labelBalance.textContent = balance + ' $';
+};
+calcDisplayBalance(account1.movements);
+
+const createUsernames = accounts => {
   accounts.forEach(acc => {
     acc.username = acc.owner
-    .toLowerCase()
-    .split(' ')
-    .map(name => name[0])
-    .join('');
-  })
-}
-
-createUsernames(accounts)
-console.log(accounts);
-
-
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -124,3 +125,42 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(movementsUSDfor);
 
 // const movementsDescriptions = movements.map((mov, i) => `Movement ${i +1}: You ${mov > 0 ? 'deposited' : ' withdrew'} ${Math.abs(mov)}`)
+
+// const deposits = movements.filter((mov) => mov > 0)
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = []
+// for (const mov of movements) {
+//   if(mov>0) depositsFor.push(mov)
+// }
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(mov => mov < 0)
+// console.log(withdrawals);
+
+// console.log(movements);
+
+// const balance = movements.reduce((acc, mov) => acc + mov, 0);
+// // const balance = movements.reduce((acc, mov, i, arr) => {
+// //   console.log(`Iteration ${i}: ${acc}`);
+// //   return acc + mov
+// // }, 0);
+// console.log(balance);
+
+// let balance2 = 0;
+// for (const mov of movements) {
+//   balance2 += mov;
+// }
+// console.log(balance2);
+
+// maximum value of the movements with reduce
+
+// const maximumVal = movements.reduce((acc, mov) => {
+//   if (acc <= mov) {
+//     return mov;
+//   }
+//   return acc;
+// }, movements[0]);
+
+// console.log(maximumVal);
